2cb79472a2e906f7fd2c905240cda530fa7c1b9d
Cristina <comalokicoma@gmail.com>
2019-07-08 17:19:10 -0500

    dia de la catastrofe
    no da el user.profile

---
+++ blog/models.py
@@ -0,0 +1,15 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+class Post(models.Model):
+    titulo = models.CharField(max_length=100)
+    contenido = models.TextField()
+    fecha = models.DateField(default=timezone.now)
+    autor = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.titulo
+        
+    
