1ec223a5fc2004e564f0fa70a0001e185f3ae34c
Cristina <comalokicoma@gmail.com>
2019-07-09 14:17:57 -0500

    Hasta Detail View
    el video de 53 min :'(

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Post
+from django.views.generic import ListView, DetailView
 
 def home(request):
     context = {
@@ -7,6 +8,14 @@ def home(request):
     }
     return render(request, 'blog/home.html', context)
 
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'
+    context_object_name = 'posts'
+    ordering = ['-fecha']
+
+class PostDetailView(DetailView):
+    model = Post
+    
 def about(request):
     return render(request, 'blog/about.html', {'title': 'About'})
-
