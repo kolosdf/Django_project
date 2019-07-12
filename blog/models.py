c5162d022029ac802ca6d34828d420194378bbd2
Cristina <comalokicoma@gmail.com>
2019-07-12 16:24:06 -0500

    :D
    email

--- blog/models.py
+++ blog/models.py
@@ -1,15 +1,16 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
-
+from django.urls import reverse
 
 class Post(models.Model):
     titulo = models.CharField(max_length=100)
     contenido = models.TextField()
-    fecha = models.DateField(default=timezone.now)
+    fecha = models.DateTimeField(auto_now_add=True)
     autor = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.titulo
-        
-    
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
